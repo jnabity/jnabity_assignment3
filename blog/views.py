fd32c659b7ad89cb3275aa4ff89bddd171881e69
jnabity <jnabity@unomaha.edu>
2017-07-31 17:59:55 -0500

    Finalized changes to assignment3.

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,52 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
+
+
+# def post_list(request):
+#   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#  return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    tone_analyzer = ToneAnalyzerV3(
+        username='1b3046d8-72a0-4271-a038-9c3023dbb663',
+        password='xxR4FaEZ1asy',
+        version='2016-05-19 ')
+
+    language_translator = LanguageTranslator(
+        username='e425fa36-2272-4a50-bf2a-fafeaff20bc4',
+        password='T3lEbdffrgZJ')
+
+    # print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+    for post in posts:
+        data = json.dumps(tone_analyzer.tone(text=post.text), indent=1)  # converting to string and storing in the data
+        j = json.loads(data);
+        post.info = j['document_tone']['tone_categories'][0]['tones']
+        # post.info = json.dumps(post.info);
+        post.angerScore = post.info[0]['score']
+        post.disgustScore = post.info[1]['score']
+        post.fearScore = post.info[2]['score']
+        post.joyScore = post.info[3]['score']
+        post.sadScore = post.info[4]['score']
+        # print(post.info[0]['tone_name'])
+        translation = language_translator.translate(
+            text=post.text,
+            source='en',
+            target='es')
+        post.translatedText = json.dumps(translation, indent=2, ensure_ascii=False)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    # Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -24,6 +62,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
